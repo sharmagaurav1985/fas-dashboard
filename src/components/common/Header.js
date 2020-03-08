43a155a9f4214ed3de156fa76753eb6795a55567
shamagaurav1985 <sharma.gaurav1985@yahoo.com>
2020-03-08 16:41:50 +0530

    test changes

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <nav>
       <a href="/">Home</a> | <a href="/numberOfClients">NumberOfClients</a> |{" "}
-      <a href="/courses">Courses</a> | <a href="/about">About</a>
+      <a href="/courses">Cours</a> | <a href="/about">About</a>
     </nav>
   );
 }
